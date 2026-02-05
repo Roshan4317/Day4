44062ed44660d774d273693222814fed98fb3aef
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-02-04 23:07:33 -0500

    Day 3 Updated

---
+++ script.js
@@ -0,0 +1,166 @@
+// const digits = [2, 4, 6, 5];
+
+// let result = 0;
+
+// digits.map((el, index) => {
+//   result += el * Math.pow(10, index);
+// });
+
+// console.log(result);
+
+// const final = digits.reduce((acc, curr, index) => {
+//   const value = curr * Math.pow(10, index);
+//   return acc + value;
+// });
+
+// console.log(final);
+
+// const num2 = [7, 3, 2];
+
+// let num2Result = 7 * 1 + 3 * 10 + 2 * 100;
+// console.log(num2Result);
+
+// function digitsToNumber(digit) {
+//   const result = digit.reduce((acc, curr, index) => {
+//     return (acc += curr * Math.pow(10, index));
+//   });
+
+//   return result;
+// }
+
+// console.log(digitsToNumber([2, 4, 6, 5]));
+
+//*************************************************************************************************
+
+//Split numbers into Digits
+
+// function numbersIntoDigits(value) {
+//   let result = [];
+//   let num = value;
+
+//   while (num / 10 > 0) {
+//     let numb = num % 10;
+//     result.unshift(numb);
+
+//     num = Math.floor(num / 10);
+//   }
+
+//   return result;
+// }
+
+// console.log(numbersIntoDigits(12345));
+
+// let num = 12345;
+
+// const resultArray = [];
+
+// while (num / 10 > 0) {
+//   const remainder = num % 10;
+//   resultArray.unshift(remainder);
+
+//   num = Math.floor(num / 10);
+// }
+
+// console.log(resultArray);
+
+// function numberIntoDigit(value) {
+//   let num = value;
+
+//   const digitArray = [];
+//   while (num >= 1) {
+//     const remainder = num % 10;
+//     digitArray.unshift(remainder);
+
+//     num = Math.floor(num / 10);
+//   }
+
+//   return digitArray;
+// }
+
+// console.log(numberIntoDigit(98765430021));
+
+//*************************************************************************************************
+
+// Reverse a number
+
+// function reverseNumber(value) {
+//   let num = value;
+//   let result = [];
+
+//   while (num > 0) {
+//     const remainder = num % 10;
+//     result.push(remainder);
+//     num = Math.floor(num / 10);
+//   }
+
+//   return +result.join("");
+// }
+
+// console.log(reverseNumber(1234));
+
+// function reverseNumber(value) {
+//   let num = value;
+//   const result = [];
+
+//   while (num > 0) {
+//     const remainder = num % 10;
+//     result.unshift(remainder);
+
+//     num = Math.floor(num / 10);
+//   }
+
+//   const reversed = result.reduce((acc, el, index) => {
+//     return (acc += el * Math.pow(10, index));
+//   }, 0);
+
+//   return reversed;
+// }
+
+// console.log(reverseNumber(98756));
+
+// function reverseNumber(value) {
+//   let number = value;
+//   const result = [];
+
+//   while (number > 0) {
+//     const remainder = number % 10;
+//     result.unshift(remainder);
+//     number = Math.floor(number / 10);
+//   }
+
+//   let totalSum = 0;
+
+//   result.map((el, index) => (totalSum += el * 10 ** index));
+
+//   return totalSum;
+// }
+
+// console.log(reverseNumber(1234));
+
+//*************************************************************************************************
+
+// Remove the decimal point Mathematically
+
+// function removeDecimalPoint(num) {
+//   let value = num;
+
+//   while (!Number.isInteger(value)) {
+//     value = value * 10;
+//   }
+
+//   return value;
+// }
+
+// console.log(removeDecimalPoint(12.34));
+
+// function removeDecimalPoint(num) {
+//   while (num !== Math.floor(num)) {
+//     num = num * 10;
+//   }
+
+//   return num;
+// }
+
+// console.log(removeDecimalPoint(12.34));
+
+//*************************************************************************************************
